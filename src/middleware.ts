2d559386152bfb240ab071f30e48d6ff55c63fb3
Selahattinozdmr <114940039+Selahattinozdmr@users.noreply.github.com>
2024-09-09 23:52:04 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,1 @@
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
 
-export default NextAuth(authConfig).auth
-
-export const config={
-    matcher:["/((?!api|static|.*\\..*|_next).*)"]
-}
