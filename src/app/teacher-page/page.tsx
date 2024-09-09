f10e8b0d9267eef9637ae078a7f50e0d9ae4a653
Selahattinozdmr <114940039+Selahattinozdmr@users.noreply.github.com>
2024-09-09 23:46:51 +0300

    Update page.tsx

--- src/app/teacher-page/page.tsx
+++ src/app/teacher-page/page.tsx
@@ -1,12 +1,1 @@
-import TeacherPage from '@/components/teacherPage/TeacherPage'
-import React from 'react'
 
-const TPage = () => {
-  return (
-    <div className=' flex' >
-        <TeacherPage />
-    </div>
-  )
-}
-
-export default TPage
