0c2fee3a6acefc842411d80ace3f6321650d0be4
Selahattinozdmr <114940039+Selahattinozdmr@users.noreply.github.com>
2024-09-09 23:42:45 +0300

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,1 @@
-import { loginAction } from '@/action'
-import LoginForm from '@/components/login/LoginForm'
-import React from 'react'
 
-const page = () => {
-  return (
-    <div>
-        <LoginForm login={loginAction}/>
-    </div>
-  )
-}
-
-export default page
