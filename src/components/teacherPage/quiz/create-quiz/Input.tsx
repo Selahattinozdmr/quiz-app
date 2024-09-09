8618cc3fda4fcc4bafa43ea44a484e782bbdca0d
Selahattinozdmr <114940039+Selahattinozdmr@users.noreply.github.com>
2024-09-09 23:50:23 +0300

    Update Input.tsx

--- src/components/teacherPage/quiz/create-quiz/Input.tsx
+++ src/components/teacherPage/quiz/create-quiz/Input.tsx
@@ -1,11 +1,1 @@
-import React from 'react'
 
-const Input = ({value}:{value:any}) => {
-  return (
-    <div>
-        <input type="text" name="mat1" value={value} className=" hidden" />
-    </div>
-  )
-}
-
-export default Input
