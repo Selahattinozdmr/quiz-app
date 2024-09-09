c8ffd1fe73b0216364cd5f949bb9b4352638183f
Selahattinozdmr <114940039+Selahattinozdmr@users.noreply.github.com>
2024-09-09 23:49:00 +0300

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,1 @@
-import Link from 'next/link'
-import React from 'react'
-import Links from "@/components/navbar/links/Links"
-import { auth } from '@/auth'
-const Navbar = async() => {
-  const session=await auth()
-    return (
-    <div className=' flex justify-around items-center'>
-      <Link href="/" className={" text-3xl"}>SeloQuiz</Link>
-    <div>
-        
-    </div>
-        <Links session={session}/>
-    </div>
-  )
-}
 
-export default Navbar
